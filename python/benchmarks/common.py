7a9907b87d77fb91592e7e29170213495a64035e
Jialue Chen <jialuechen@outlook.com>
2024-02-25 14:17:14 -0500

    restructured

--- python/benchmarks/common.py
+++ python/benchmarks/common.py
@@ -4,11 +4,6 @@ import numpy as np
 
 
 def generate_pseudo_random_dataframe(n, freq="s", end_timestamp="1/1/2023"):
-    """
-    Generates a Data Frame with 2 columns (timestamp and value) and N rows
-    - timestamp contains timestamps with a given frequency that end at end_timestamp
-    - value contains random floats that sum up to approximately N, for easier testing/verifying
-    """
     # Generate random values such that their sum is equal to N
     values = np.random.uniform(0, 2, size=n)
     # Generate timestamps
@@ -20,11 +15,7 @@ def generate_pseudo_random_dataframe(n, freq="s", end_timestamp="1/1/2023"):
 
 
 def generate_random_floats_dataframe(num_rows, num_cols):
-    """
-    Generates a dataframe of random 64 bit floats with num_rows rows and num_cols columns.
-    Columns are named "col_n" for n in range(num_cols).
-    Row range indexed.
-    """
+    
     columns = [f"col_{n}" for n in range(num_cols)]
     rng = np.random.default_rng()
     data = rng.random((num_rows, num_cols), dtype=np.float64)
@@ -34,7 +25,6 @@ def generate_random_floats_dataframe(num_rows, num_cols):
 def generate_benchmark_df(n, freq="min", end_timestamp="1/1/2023"):
     timestamps = pd.date_range(end=end_timestamp, periods=n, freq=freq)
     k = n // 10
-    # Based on https://github.com/duckdblabs/db-benchmark/blob/master/_data/groupby-datagen.R#L19
     dt = pd.DataFrame()
     dt["id1"] = np.random.choice([f"id{str(i).zfill(3)}" for i in range(1, k + 1)], n)
     dt["id2"] = np.random.choice([f"id{str(i).zfill(3)}" for i in range(1, k + 1)], n)
